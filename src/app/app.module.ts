5272c6f05334ce255cc3111f4cd8903f6d93ea5c
votrebanque <ismailalabouhp@gmail.com>
2022-09-22 15:27:23 +0000

    chapter 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    EventsAppComponent
+  ],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
---
+++ src/app/events-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `<h1>Hello World!</h1>
+  <img src="/assets/images/basic-shield.png" />
+  `
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
